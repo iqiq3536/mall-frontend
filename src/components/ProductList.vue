ffa33f130000d4f80a3cff2cb93504e2b52736d1
tbw <1147223321@qq.com>
2025-01-14 22:50:06 +0800

    tbw

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -62,7 +62,7 @@ export default {
 
         // 提取需要的字段
         const newItems = response.data.map(item => ({
-          id: item.id,
+
           name: item.name,
           price: item.price,
           img_url: 'https://img2.baidu.com/it/u=1225084252,536866490&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=1309',
